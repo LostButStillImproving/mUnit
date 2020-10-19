673368807363aeb789a7ba36d330f772ae55f3f3
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-10-19 14:15:07 +0200

    Removed static subclases in favor of pure functions

--- src/TestOrchestrator.java
+++ src/TestOrchestrator.java
@@ -5,37 +5,29 @@ import java.util.Map;
 
 // TODO add async
 abstract class TestOrchestrator {
-    private static class TestMethodGetter {
-        static Method[] getTestMethods() throws ClassNotFoundException {
-            Class<?> testSuit = Class.forName("TestSuite");
-
-            return testSuit.getDeclaredMethods();
-        }
+    private static Method[] getTestMethods() throws ClassNotFoundException {
+        Class<?> testSuit = Class.forName("TestSuite");
+        return testSuit.getDeclaredMethods();
     }
-    private static class TestMethodInvoker {
-        private static Map<String, String> buildResults(Method[] methods) {
-            Map<String,String> methodNamesOutputsMap = new HashMap<>();
-            for (Method method:methods
-            ) {
-                try {
-                    methodNamesOutputsMap.put(method.getName(), method.invoke(methods).toString());
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
+    private static Map<String, String> buildResults(Method[] methods) {
+        Map<String,String> methodNamesOutputsMap = new HashMap<>();
+        for (Method method:methods
+        ) {
+            try {
+                methodNamesOutputsMap.put(method.getName(), method.invoke(methods).toString());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
             }
-
-            return methodNamesOutputsMap;
         }
+        return methodNamesOutputsMap;
     }
-    private static class Summarizer{
-        static void printSummary(Map<String, String> mapOfMethodNamesAndOutputs){
-            mapOfMethodNamesAndOutputs.forEach((k, v) -> System.out.printf( "%-15s %15s %n",k,v));
-        }
+
+    private static void printSummary(Map<String, String> mapOfMethodNamesAndOutputs){
+        mapOfMethodNamesAndOutputs.forEach((k, v) -> System.out.printf( "%-15s %15s %n",k,v));
     }
     static void runTestSuite() {
         try {
-            Summarizer.printSummary(TestMethodInvoker.buildResults(
-                                                     TestMethodGetter.getTestMethods()));
+            printSummary(buildResults(getTestMethods()));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
